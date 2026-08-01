c48e1edc6948c0dc49ae134458611c18f912d958
Sofia Bravim <s252273@dac.unicamp.br>
2026-07-31 21:48:43 -0300

    feat: adiciona ícones nas abas e telas provisórias

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,24 +1,40 @@
-import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-SplashScreen.preventAutoHideAsync();
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons'; // Importa a biblioteca de ícones
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  useEffect(() => {
-    SplashScreen.hideAsync();
-  }, []);
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <Tabs screenOptions={{ tabBarActiveTintColor: '#26b845' }}>
+      
+      <Tabs.Screen 
+        name="index" 
+        options={{ 
+          title: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }} 
+      />
+      
+      <Tabs.Screen 
+        name="greenhouse" 
+        options={{ 
+          title: 'Estufa',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="leaf-outline" size={size} color={color} />
+          ),
+        }} 
+      />
+      
+      <Tabs.Screen 
+        name="agenda" 
+        options={{ 
+          title: 'Agenda',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="checkbox-outline" size={size} color={color} />
+          ),
+        }} 
+      />
+      
+    </Tabs>
   );
 }
