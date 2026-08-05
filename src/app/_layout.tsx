f271694de035f0a84e73db4be7c6dbcd55d3c18e
Sofia Bravim <s252273@dac.unicamp.br>
2026-08-05 15:13:38 -0300

    fix: atualização das cores e outros detalhes do cabeçalho e rodapé

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,7 +5,23 @@ import { AppProvider } from '../context/AppContext'; // 1. Importou o provedor
 export default function TabLayout() {
   return (
     <AppProvider> 
-      <Tabs screenOptions={{ tabBarActiveTintColor: '#26b845' }}>
+      <Tabs screenOptions={{ 
+        tabBarActiveTintColor: '#26b845', //ícones do rodapé
+        tabBarInactiveTintColor: '#9c978f', //ícones inativos do rodapé
+        tabBarStyle: {
+          backgroundColor: '#fcecb5', //fundo do rodapé
+          borderTopColor: '#E8E1D5', //linha que separa o rodapé
+          elevation: 0, //tira a sombra do android
+          shadowOpacity: 0, //tira a sombra do ios
+        },
+        headerStyle: {
+          backgroundColor: '#fcecb5', //fundo do cabeçalho
+          elevation: 0,
+          shadowOpacity: 0,
+        },
+        headerTintColor: '#4A3F35', //cor do título do cabeçalho
+        headerTitleAlign: 'center', //centraliza o conteúdo
+        }}>
         
         <Tabs.Screen 
           name="index" 
