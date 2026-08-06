48d973748ba32410ac98181d0bcde6689253eea5
Sofia Bravim <s252273@dac.unicamp.br>
2026-08-05 23:37:11 -0300

    feat: arquivos de preparação de imagens png e constantes das imagens

---
+++ src/constants/plants.ts
@@ -0,0 +1,8 @@
+export const PLANT_IMAGES: Record<string, Record<number, any>> = {
+  girassol: {
+    0: require('../../assets/plants/sunflower/stage_0.png'),
+    1: require('../../assets/plants/sunflower/stage_1g.png'),
+    2: require('../../assets/plants/sunflower/stage_2g.png'),
+    3: require('../../assets/plants/sunflower/stage_3g.png'),
+  },
+};
